ed6d134e947342a4537144cb197b4359e0967c09
Joao Marcos <joao.barroso@alu.ufc.br>
2024-09-04 08:05:03 -0300

    feat: next-auth and axios

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,105 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import axios from "axios";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: 7 * 24 * 60 * 60, // 7 days
+  },
+  callbacks: {
+    async signIn({ user, profile }: { user: any; profile?: any }) {
+      const { email, given_name, family_name, picture } = profile;
+
+      try {
+        // Verify if the user is already registered
+        const loginResponse = await axios.post(
+          "http://localhost:8000/auth/google/login",
+          { email },
+          { headers: { "Content-Type": "application/json" } },
+        );
+
+        if (loginResponse.data) {
+          user.jwt = loginResponse.data;
+          return true;
+        }
+      } catch (error) {
+        // If the user is not registered, register them
+        if (axios.isAxiosError(error) && error.response) {
+          if (
+            error.response.status === 400 &&
+            error.response.data.message === "Usuário não encontrado."
+          ) {
+            return await registerUser({
+              email,
+              given_name,
+              family_name,
+              picture,
+              user,
+            });
+          }
+        }
+        return false;
+      }
+      return true;
+    },
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      session.user = {
+        ...session.user,
+        id: token.id as string,
+        email: token.email as string,
+        name: token.name as string,
+        image: token.picture as string,
+        jwt: token.jwt as string,
+      };
+      return session;
+    },
+  },
+});
+
+async function registerUser({
+  email,
+  given_name,
+  family_name,
+  picture,
+  user,
+}: any) {
+  try {
+    const registerResponse = await axios.post(
+      "http://localhost:8000/auth/google/register",
+      {
+        user: {
+          first_name: given_name,
+          last_name: family_name,
+          avatar_url: picture,
+          level_id: 1,
+        },
+        login: {
+          email,
+          is_google_user: true,
+        },
+      },
+      { headers: { "Content-Type": "application/json" } },
+    );
+
+    if (registerResponse.data) {
+      user.jwt = registerResponse.data;
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+export { handler as GET, handler as POST };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
+import Provider from "@/app/Provider";
 
 // Fonte usada no nosso projeto é a Nunito
 const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "modoke",
-  description: "Apredendo sobre acessibilidade web de forma simples e divertida",
+  description:
+    "Apredendo sobre acessibilidade web de forma simples e divertida",
 };
 
 export default function RootLayout({
@@ -16,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={font.className}>
+        <Provider>{children}</Provider>
+      </body>
     </html>
   );
 }
