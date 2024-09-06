7ead28abf0cce444d2db745d5cbf9251cee6ba52
Joao Marcos <joao.barroso@alu.ufc.br>
2024-09-06 17:27:43 -0300

    feat: question loading page

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -8,7 +8,7 @@ interface SpinnerProps {
 const Spinner = ({ size, thickness }: SpinnerProps) => {
   return (
     <div
-      className={`inline-block h-16 w-h-16 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white`}
+      className={`inline-block h-${size ?? 4} w-${size ?? 4} animate-spin rounded-full border-${thickness ?? 2} border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white`}
       role="status"
     >
       <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
