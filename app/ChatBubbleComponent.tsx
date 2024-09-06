d7370aef25a8f3b19292ee2450993322969d3f99
Joao Marcos <joao.barroso@alu.ufc.br>
2024-09-06 17:50:18 -0300

    refactor: font size

--- app/ChatBubbleComponent.tsx
+++ app/ChatBubbleComponent.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 function ChatBubbleComponent({ content }: Props) {
   return (
-    <div className="flex flex-col w-fit max-w-[750px] p-4 border-gray-200 bg-green-200 rounded-e-3xl rounded-es-3xl dark:bg-green-700">
-      <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">
+    <div className="flex flex-col w-fit max-w-[750px] p-5 border-gray-200 bg-green-200 rounded-e-3xl rounded-es-3xl dark:bg-green-700">
+      <p className="text-2xl font-normal py-2.5 text-gray-900 dark:text-white">
         {content}
       </p>
     </div>
