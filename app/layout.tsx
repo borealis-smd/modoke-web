6ae1ee0455c829eee3600cc30e70e5371e37759f
leticiar-io <leticia.rodrigues20019@gmail.com>
2024-09-06 21:36:20 -0300

    style: change project font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Provider from "@/app/Provider";
 
-// Fonte usada no nosso projeto é a Nunito
-const font = Nunito({ subsets: ["latin"] });
+// Fonte usada no nosso projeto é a Poppins
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "modoke",
