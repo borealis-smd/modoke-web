59f975d52512e0e950d3eaf835fdf69d41192930
leticiar-io <leticia.rodrigues20019@gmail.com>
2024-09-07 18:42:59 -0300

    style: improving the banner component

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,18 +1,30 @@
 type Props = {
    title: string;
+   subtitle?: string;
+   variant: "session" | "revision";
    //image: string;
 }
 
-export const Banner = ({ title }: Props) => {
+export const Banner = ({ title, variant }: Props) => {
    return (
       <div
          className="bg-green-700 w-full h-[8rem] lg:h-[10.5rem] flex items-center text-white px-6 py-5 rounded-lg"
          role="banner"
          aria-labelledby="section-header"
       >
-         <h1 id="section-header" className="text-2xl lg:text-4xl font-semibold">
-            {title}
-         </h1>
+         {variant === "session" ? (
+            <h1 id="section-header" className="text-2xl lg:text-4xl font-semibold">
+               {title}
+            </h1>
+         ) : (
+            <div className="flex flex-col space-y-2">
+               <span className="font-medium text-md">Hey, @Modoke!</span>
+               <h2 id="section-header" className="text-2xl lg:text-4xl font-semibold">
+                  {title}
+               </h2>
+            </div>
+         )
+         }
       </div>
    );
 };
