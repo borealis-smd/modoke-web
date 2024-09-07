bdd146718ca4ad7ed855fd8d6ca72b92511f6291
leticiar-io <leticia.rodrigues20019@gmail.com>
2024-09-07 19:51:21 -0300

    refactor: improving the input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,19 +1,26 @@
+"use client";
+
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-const inputStyles = "block w-full px-4 py-2 rounded-xl text-sm font-medium text-slate-500 bg-transparent border border-slate-300 focus:border-teal-500 focus:ring focus:ring-teal-500/25 focus:outline-none";
+const inputStyles =
+  "block w-full px-4 py-2 rounded-xl text-sm font-medium text-slate-500 bg-transparent border border-slate-300 focus:border-teal-500 focus:ring focus:ring-teal-500/25 focus:outline-none";
 const containerStyles = "relative flex items-center";
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   placeholder?: string;
   type?: "text" | "password" | "email" | "number" | "tel" | "url";
   name?: string;
-  icon?: React.ReactNode;
-  onIconClick?: () => void; // Função para lidar com o clique do ícone
+  icon?: React.ReactNode; // Ícone opcional
+  onIconClick?: () => void; // Função para lidar com o clique do ícone (se houver)
+  isIconButton?: boolean; // Define se o ícone deve ser um botão ou apenas visual
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, placeholder, type = "text", icon, onIconClick, ...props }, ref) => {
+  (
+    { className, placeholder, type = "text", icon, onIconClick, isIconButton = true, ...props },
+    ref
+  ) => {
     const [isPasswordVisible, setIsPasswordVisible] = React.useState(type === "password");
 
     const handleIconClick = () => {
@@ -30,12 +37,13 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             "pr-10": icon, // Adiciona padding extra à direita se houver ícone
           })}
           ref={ref}
-          type={isPasswordVisible ? "text" : "password"}
+          type={isPasswordVisible && type === "password" ? "text" : type}
           placeholder={placeholder}
           {...props}
           aria-label={props["aria-label"]}
         />
-        {icon && (
+        {icon && isIconButton ? (
+          // Renderiza o ícone como um botão se isIconButton for verdadeiro
           <button
             type="button"
             className="absolute inset-y-0 right-0 flex items-center pr-3"
@@ -44,6 +52,11 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           >
             {icon}
           </button>
+        ) : (
+          // Renderiza o ícone apenas como um elemento visual se isIconButton for falso
+          <span className="absolute inset-y-0 right-0 flex items-center pr-3">
+            {icon}
+          </span>
         )}
       </div>
     );
