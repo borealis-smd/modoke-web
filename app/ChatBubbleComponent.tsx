6926c503e4cc5855b4bebc31fe81d873d4159b86
Joao Marcos <joao.barroso@alu.ufc.br>
2024-09-25 09:12:42 -0300

    refactor: extract parseContent function

--- app/ChatBubbleComponent.tsx
+++ app/ChatBubbleComponent.tsx
@@ -1,58 +1,12 @@
 import React from "react";
-import DOMPurify from "isomorphic-dompurify";
-import CodeBlockComponent from "./lesson/CodeBlockComponent";
 import { cn } from "@/lib/utils";
 import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import { parseContent } from "@/lib/parseContent";
 
 interface Props {
   content: string;
   variant?: "chat" | "title" | "disclaimer";
   className?: string;
-}
-
-function parseContent(content: string) {
-  const elements: Array<JSX.Element> = [];
-  let lastIndex = 0;
-  const regex = /<Code language='(.*?)'>([\s\S]*?)<\/Code>/g;
-  let match;
-
-  while ((match = regex.exec(content)) !== null) {
-    const beforeCode = content.slice(lastIndex, match.index);
-
-    if (beforeCode) {
-      const sanitizedContent = DOMPurify.sanitize(beforeCode);
-      elements.push(
-        <div
-          key={lastIndex}
-          dangerouslySetInnerHTML={{ __html: sanitizedContent }}
-          className="mb-3"
-        />
-      );
-    }
-
-    const language = match[1].trim();
-    const code = match[2].trim();
-    elements.push(
-      <CodeBlockComponent key={match.index} code={code} language={language} />
-    );
-
-    lastIndex = match.index + match[0].length;
-  }
-
-  const remainingContent = content.slice(lastIndex);
-  if (remainingContent) {
-    const sanitizedContent = DOMPurify.sanitize(remainingContent);
-    elements.push(
-      <div
-        key={lastIndex}
-        dangerouslySetInnerHTML={{
-          __html: sanitizedContent,
-        }}
-      />
-    );
-  }
-
-  return elements;
 }
 
 function ChatBubbleComponent({ content, variant = "chat", className }: Props) {
